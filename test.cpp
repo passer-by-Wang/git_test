116d28abd8846d464f276599a1d6fcbc3e852b61
Wang Hua <2273425240@qq.com>
2025-05-02 11:48:02 +0800

    在 test.cpp 中添加了基本的数学运算函数，包括加法、减法、乘法、取模、幂运算和平方根运算。

--- test.cpp
+++ test.cpp
@@ -4,3 +4,15 @@ int main() {
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
+
+int add(int a, int b) { return a + b; }
+
+int sub(int a, int b) { return a - b; }
+
+int mul(int a, int b) { return a * b; }
+
+int mod(int a, int b) { return a % b; }
+
+int pow(int a, int b) { return pow(a, b); }
+
+int sqrt(int a) { return sqrt(a); }
